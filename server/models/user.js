a373ba51ff99060065ca3bc6e20394a7f3c2265e
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-06 13:35:29 +0100

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: 'String',
+    required: true,
+    validate: {
+      validator: function(value) {
+        return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
+      },
+      message: '{VALUE} is not a valid email!'
+    }
+  }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = { User };
