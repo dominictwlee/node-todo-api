3b7134e5d771978122bbded4ed28d1786460b173
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-06 16:09:56 +0100

    Added GET /todos test and seed data

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const expect = require('expect');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const seedTodos = [{
+  text: 'first test todo',
+}, {
+  text: 'second test todo',
+}, {
+  text: 'third test todo',
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => Todo.insertMany(seedTodos))
+    .then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -13,39 +23,50 @@ describe('POST /todos', () => {
     const text = 'Test todo';
 
     request(app)
-    .post('/todos')
-    .send({ text })
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.text).toBe(text);
-    })
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
 
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
-        done();
-      }).catch( err => done(err));
+        Todo.find({ text }).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch(err => done(err));
+      });
+  });
 
+  describe('GET /todos', () => {
+    it('should get a list of todos', (done) => {
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(3);
+        })
+        .end(done);
     });
   });
 
   it('should not create todo with invalid body data', (done) => {
     request(app)
-    .post('/todos')
-    .send({})
-    .expect(400)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
-        done();
-      }).catch( err => done(err));
-    })
-  })
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(3);
+          done();
+        }).catch(err => done(err));
+      });
+  });
 });
