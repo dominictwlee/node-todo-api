13e8f89d6f05018e26661c9e392af512ac741a76
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-06 18:58:46 +0100

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -25,6 +26,20 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => res.send({ todos }))
+    .catch(err => res.status(400).send(err));
+});
+
+//  GET todos: Query by ID
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
     .catch(err => res.status(400).send(err));
 });
 
