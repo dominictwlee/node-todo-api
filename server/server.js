5e2d32fb251e8cf8a9abb5c0d8d8e3a9f1360ca7
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-06 17:36:17 +0100

    Added mongoose queries playground file

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
+    completed: req.body.completed,
   });
   todo.save()
     .then(doc => res.send(doc))
