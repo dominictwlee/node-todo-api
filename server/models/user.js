c54d19ddee05ba682f800473e5923e024e36b6cf
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-10 11:58:35 +0100

    Setup validation for email and password

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,32 @@
+const validator = require('validator');
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: 'String',
     required: true,
+    unique: true,
     validate: {
-      validator: function emailValidate(value) {
-        return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
-      },
+      validator: validator.isEmail,
       message: '{VALUE} is not a valid email!',
     },
   },
+  password: {
+    type: 'String',
+    required: true,
+    minlength: 8,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
 });
 
 const User = mongoose.model('user', userSchema);
