db66ebffbab0d091c6e16926d0532a24be0ccb3b
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-11 19:07:17 +0100

    change config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
 
 if (env === 'development' || env === 'test') {
+  const config = require('./config.json');
   const envConfig = config[env];
   //  Put keys into an array and loop through them
   Object.keys(envConfig).forEach((key) => {
