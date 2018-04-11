f90ba5dcb9784305acbe5881be83783d93888ae7
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-11 18:27:15 +0100

    Added comments and put config variable in global

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
+const config = require('./config.json');
 
 if (env === 'development' || env === 'test') {
-  const config = require('./config.json');
   const envConfig = config[env];
 
-  //  turns properties into array
+  //  Put keys into an array and loop through them
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
   });
