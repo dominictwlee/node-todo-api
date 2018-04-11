23a93203ee6af0de3e350781450d8d5ed1f88349
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-11 18:28:24 +0100

    Got rid of whitespace

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,6 @@ const config = require('./config.json');
 
 if (env === 'development' || env === 'test') {
   const envConfig = config[env];
-
   //  Put keys into an array and loop through them
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
