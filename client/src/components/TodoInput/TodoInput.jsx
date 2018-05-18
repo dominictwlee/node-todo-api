423f5b1e1ae034bedd750a459c1f66dae90fab8f
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-18 01:59:03 +0100

    Style text input

--- client/src/components/TodoInput/TodoInput.jsx
+++ client/src/components/TodoInput/TodoInput.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddIcon from '@material-ui/icons/Add';
 
+import styles from './todoInput.css';
+
 const TodoInput = props => (
-  <form onSubmit={props.handleAdd}>
-    <label htmlFor="task">
-      Add A Task:
-      <input
-        id="task"
-        name="task"
-        type="text"
-        value={props.task}
-        onChange={props.handleInputChange}
-        placeholder="e.g. Go through React docs"
-      />
-    </label>
+  <form onSubmit={props.handleAdd} className={styles.form}>
+    <input
+      className={styles.text}
+      name="task"
+      type="text"
+      value={props.task}
+      onChange={props.handleInputChange}
+      placeholder="e.g. Go through React docs"
+    />
+
     <button type="submit">
       <AddIcon style={{ fontSize: 36 }} nativeColor="black" />
     </button>
